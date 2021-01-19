a66f72222e85365aa95c872a3a516a053515b8b3
Caio <caio.vincenzo@estudante.ufjf.br>
2021-01-18 21:59:34 -0300

    Adiciona rodas

--- works/trabalho01.js
+++ works/trabalho01.js
@@ -1,47 +1,45 @@
-function main()
-{
-  var stats = initStats();          // To show FPS information
-  var scene = new THREE.Scene();    // Create main scene
-  var renderer = initRenderer();    // View function in util/utils
-  var camera = initCamera(new THREE.Vector3(0, -30, 15)); // Init camera in this position
-  var light  = initDefaultLighting(scene, new THREE.Vector3(7, 7, 7));
+function main() {
+    var stats = initStats(); // To show FPS information
+    var scene = new THREE.Scene(); // Create main scene
+    var renderer = initRenderer(); // View function in util/utils
+    var camera = initCamera(new THREE.Vector3(0, -30, 15)); // Init camera in this position
+    var light = initDefaultLighting(scene, new THREE.Vector3(7, 7, 7));
 
-  // Enable mouse rotation, pan, zoom etc.
-  var trackballControls = new THREE.TrackballControls( camera, renderer.domElement );
+    // Enable mouse rotation, pan, zoom etc.
+    var trackballControls = new THREE.TrackballControls(
+        camera,
+        renderer.domElement
+    );
 
-  // Show axes (parameter is size of each axis)
-  var axesHelper = new THREE.AxesHelper( 12 );
-  scene.add( axesHelper );
+    // Show axes (parameter is size of each axis)
+    var axesHelper = new THREE.AxesHelper(12);
+    scene.add(axesHelper);
 
-  // create the ground plane
-  var planeGeometry = new THREE.PlaneGeometry(20, 20);
-  planeGeometry.translate(0.0, 0.0, -0.02); // To avoid conflict with the axeshelper
-  var planeMaterial = new THREE.MeshBasicMaterial({
-      color: "rgba(150, 150, 150)",
-      side: THREE.DoubleSide,
-  });
-  var plane = new THREE.Mesh(planeGeometry, planeMaterial);
-  // add the plane to the scene
-  scene.add(plane);
+    // create the ground plane
+    var planeGeometry = new THREE.PlaneGeometry(20, 20);
+    planeGeometry.translate(0.0, 0.0, -0.02); // To avoid conflict with the axeshelper
+    var planeMaterial = new THREE.MeshBasicMaterial({
+        color: "rgba(150, 150, 150)",
+        side: THREE.DoubleSide,
+    });
+    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    // add the plane to the scene
+    scene.add(plane);
 
-  var cubeGeometry = new THREE.BoxGeometry(4, 2, 1);
-  var cubeMaterial = new THREE.MeshPhongMaterial();
-  var mainCube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-  mainCube.position.set(0.0, 0.0, 2);
-  
-  var cube = createCube(2, 5, 1);
-  cube.translateX(2)
-  cube.translateZ(-1)
-  var cube2 = createCube(2, 5, 1);
-  cube2.translateX(-2)
-  cube2.translateZ(-1)
-  mainCube.add(cube);
-  mainCube.add(cube2);
-  scene.add(mainCube);
-  // scene.add(cube2);
+    var posicaoFinal = {
+        x: 0,
+        y: 0,
+        z: 2,
+    };
 
-  // Use this to show information onscreen
-  controls = new InfoBox();
+    var kart = createKart();
+    var object = kart.object;
+    kart = kart.main;
+    scene.add(kart);
+    buildInterface(object);
+
+    // Use this to show information onscreen
+    controls = new InfoBox();
     controls.add("Basic Scene");
     controls.addParagraph();
     controls.add("Use mouse to interact:");
@@ -50,27 +48,153 @@ function main()
     controls.add("* Scroll to zoom in/out.");
     controls.show();
 
-  // Listen window size changes
-  window.addEventListener( 'resize', function(){onWindowResize(camera, renderer)}, false );
+    // Listen window size changes
+    window.addEventListener(
+        "resize",
+        function () {
+            onWindowResize(camera, renderer);
+        },
+        false
+    );
 
-  render();
-  function render()
-  {
-    stats.update(); // Update FPS
-    trackballControls.update(); // Enable mouse movements
-    // lightFollowingCamera(light, camera);
-    requestAnimationFrame(render);
-    renderer.render(scene, camera) // Render scene
-  }
-}
+    render();
+    function render() {
+        stats.update(); // Update FPS
+        trackballControls.update(); // Enable mouse movements
+        // lightFollowingCamera(light, camera);
+        moveObject(object);
+        requestAnimationFrame(render);
+        renderer.render(scene, camera); // Render scene
+    }
 
-function createCube(width, height, depth, parent){
-    // Criando o kart
-    var cubeGeometry = new THREE.BoxGeometry(width, height, depth);
-    var cubeMaterial = new THREE.MeshPhongMaterial();
-    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
-    // position the cube
-    cube.position.set(0.0, 0.0, 0.0);
-    // add the cube to the scene
-    return cube;
+    function createCube(width, height, depth, parent) {
+        const geometry = new THREE.BoxGeometry(width, height, depth);
+        const material = new THREE.MeshPhongMaterial();
+        const cube = new THREE.Mesh(geometry, material);
+        cube.position.set(0.0, 0.0, 0.0);
+        return cube;
+    }
+
+    function createCylinder(radiusTop, radiusBottom, height, radialSegments) {
+        const geometry = new THREE.CylinderGeometry(
+            radiusTop,
+            radiusBottom,
+            height,
+            radialSegments
+        );
+        const material = new THREE.MeshPhongMaterial({ color: 0xffff00 });
+        const cylinder = new THREE.Mesh(geometry, material);
+        cylinder.position.set(0.0, 0.0, 0.0);
+        return cylinder;
+    }
+
+    function createTorus(radius, tube, radialSegments, tubularSegments) {
+        const geometry = new THREE.TorusGeometry(
+            radius,
+            tube,
+            radialSegments,
+            tubularSegments
+        );
+        const material = new THREE.MeshPhongMaterial();
+        const torus = new THREE.Mesh(geometry, material);
+        torus.position.set(0.0, 0.0, 0.0);
+        return torus;
+    }
+
+    function createKart() {
+        var mainCube = createCube(4, 2, 1);
+        mainCube.position.set(0.0, 0.0, 1.5);
+        
+        // var cube = createCube(1, 3, 1);
+        // cube.translateX(2);
+        // cube.translateZ(-1);
+        
+        // var cube2 = createCube(1, 3, 1);
+        // cube2.translateX(-2);
+        // cube2.translateZ(-1);
+        
+        var frontWheels = createWheels();
+        frontWheels.position.set(2.0, 0.0, -0.5);
+        // frontWheels.translateX(2);
+        var backWheels = createWheels();
+        backWheels.position.set(-2.0, 0.0, -0.5);
+        
+        // mainCube.add(cube);
+        // mainCube.add(cube2);
+        mainCube.add(frontWheels);
+        mainCube.add(backWheels);
+        return {
+            main: mainCube,
+            object: null,
+        };
+    }
+
+    function createWheels() {
+        let cylinderRadius = 0.25;
+        let cylinderHeight = 4.5;
+        let torusRadius = 0.5;
+        let segments = 32;
+        let tube = 0.25;
+        const cylinder = createCylinder(
+            cylinderRadius,
+            cylinderRadius,
+            cylinderHeight,
+            segments
+        );
+        const leftWheel = createTorus(torusRadius, tube, segments, segments);
+        leftWheel.rotation.set(degreesToRadians(90), 0, 0);
+        leftWheel.position.set(0, cylinderHeight/2 - tube, 0);
+
+        const rightWheel = createTorus(torusRadius, tube, segments, segments);
+        rightWheel.position.set(0, -(cylinderHeight/2 - tube), 0);
+        rightWheel.rotation.set(degreesToRadians(90), 0, 0);
+
+        cylinder.add(leftWheel);
+        cylinder.add(rightWheel);
+        return cylinder;
+    }
+
+    function moveObject(object) {
+        if (!object) return;
+        object.position.set(posicaoFinal.x, posicaoFinal.y, posicaoFinal.z);
+    }
+
+    function buildInterface(object) {
+        var controls = new (function () {
+            this.x = object ? object.position.x : 0;
+            this.y = object ? object.position.y : 0;
+            this.z = object ? object.position.z : 0;
+
+            this.move = function () {
+                posicaoFinal.x = this.x;
+                posicaoFinal.y = this.y;
+                posicaoFinal.z = this.z;
+            };
+        })();
+
+        var obj = {
+            Botao: function () {
+                console.log("Apertou no botão");
+            },
+        };
+
+        // GUI interface
+        var gui = new dat.GUI();
+        gui.add(controls, "x", -5.0, 5.0)
+            .onChange(function (e) {
+                controls.move();
+            })
+            .name("X");
+        gui.add(controls, "y", -5.0, 5.0)
+            .onChange(function (e) {
+                controls.move();
+            })
+            .name("Y");
+        gui.add(controls, "z", -5.0, 5.0)
+            .onChange(function (e) {
+                controls.move();
+            })
+            .name("Z");
+        gui.add(obj, "Botao");
+    }
 }
