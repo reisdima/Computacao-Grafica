de8be0301020a2048f9ffcd372672533f90bc136
Caio <caio.vincenzo@estudante.ufjf.br>
2021-01-19 20:42:25 -0300

    Habilita movimento basico

--- works/trabalho01.js
+++ works/trabalho01.js
@@ -4,6 +4,8 @@ function main() {
     var renderer = initRenderer(); // View function in util/utils
     var camera = initCamera(new THREE.Vector3(0, -30, 15)); // Init camera in this position
     var light = initDefaultLighting(scene, new THREE.Vector3(7, 7, 7));
+    var clock = new THREE.Clock();
+    var keyboard = new KeyboardState();
 
     // Enable mouse rotation, pan, zoom etc.
     var trackballControls = new THREE.TrackballControls(
@@ -15,26 +17,25 @@ function main() {
     var axesHelper = new THREE.AxesHelper(12);
     scene.add(axesHelper);
 
-   
-  //---------------------------------------------------------------------------------------
-  // create the ground plane with wireframe
-  var planeGeometry = new THREE.PlaneGeometry(700, 700, 40, 40);
-  planeGeometry.translate(0.0, 0.0, -0.02); // To avoid conflict with the axeshelper
-  var planeMaterial = new THREE.MeshBasicMaterial({
-      color: "rgba(20, 30, 110)",
-      side: THREE.DoubleSide,
-      polygonOffset: true,
-      polygonOffsetFactor: 1, // positive value pushes polygon further away
-      polygonOffsetUnits: 1
-  });
-  var plane = new THREE.Mesh(planeGeometry, planeMaterial);
-  scene.add(plane);
+    //---------------------------------------------------------------------------------------
+    // create the ground plane with wireframe
+    var planeGeometry = new THREE.PlaneGeometry(700, 700, 40, 40);
+    planeGeometry.translate(0.0, 0.0, -0.02); // To avoid conflict with the axeshelper
+    var planeMaterial = new THREE.MeshBasicMaterial({
+        color: "rgba(20, 30, 110)",
+        side: THREE.DoubleSide,
+        polygonOffset: true,
+        polygonOffsetFactor: 1, // positive value pushes polygon further away
+        polygonOffsetUnits: 1,
+    });
+    var plane = new THREE.Mesh(planeGeometry, planeMaterial);
+    scene.add(plane);
 
-  var wireframe = new THREE.WireframeGeometry( planeGeometry );
-  var line = new THREE.LineSegments( wireframe );
-  line.material.color.setStyle( "rgb(180, 180, 180)" );  
-  scene.add(line);
-  //---------------------------------------------------------------------------------------
+    var wireframe = new THREE.WireframeGeometry(planeGeometry);
+    var line = new THREE.LineSegments(wireframe);
+    line.material.color.setStyle("rgb(180, 180, 180)");
+    scene.add(line);
+    //---------------------------------------------------------------------------------------
 
     var posicaoFinal = {
         x: 0,
@@ -72,6 +73,7 @@ function main() {
         stats.update(); // Update FPS
         trackballControls.update(); // Enable mouse movements
         // lightFollowingCamera(light, camera);
+        keyboardUpdate();
         moveObject(object);
         requestAnimationFrame(render);
         renderer.render(scene, camera); // Render scene
@@ -114,23 +116,22 @@ function main() {
     function createKart() {
         var mainCube = createCube(4, 2, 1);
         mainCube.position.set(0.0, 0.0, 1.25);
-        
+
         // var cube = createCube(1, 3, 1);
         // cube.translateX(2);
         // cube.translateZ(-1);
-        
+
         // var cube2 = createCube(1, 3, 1);
         // cube2.translateX(-2);
         // cube2.translateZ(-1);
-        
+
         var frontWheels = createWheels();
         frontWheels.position.set(2.0, 0.0, -0.5);
         // frontWheels.translateX(2);
         var backWheels = createWheels();
         backWheels.position.set(-2.0, 0.0, -0.5);
-        
-        // mainCube.add(cube);
-        // mainCube.add(cube2);
+
+        mainCube.rotation.set(0, 0, degreesToRadians(90));
         mainCube.add(frontWheels);
         mainCube.add(backWheels);
         return {
@@ -153,10 +154,10 @@ function main() {
         );
         const leftWheel = createTorus(torusRadius, tube, segments, segments);
         leftWheel.rotation.set(degreesToRadians(90), 0, 0);
-        leftWheel.position.set(0, cylinderHeight/2 - tube, 0);
+        leftWheel.position.set(0, cylinderHeight / 2 - tube, 0);
 
         const rightWheel = createTorus(torusRadius, tube, segments, segments);
-        rightWheel.position.set(0, -(cylinderHeight/2 - tube), 0);
+        rightWheel.position.set(0, -(cylinderHeight / 2 - tube), 0);
         rightWheel.rotation.set(degreesToRadians(90), 0, 0);
 
         cylinder.add(leftWheel);
@@ -206,5 +207,17 @@ function main() {
             })
             .name("Z");
         gui.add(obj, "Botao");
+    }
+
+    function keyboardUpdate() {
+        keyboard.update();
+
+        var speed = 30;
+        var moveDistance = speed * clock.getDelta();
+
+        if (keyboard.pressed("W")) kart.translateX(moveDistance);
+        if (keyboard.pressed("S")) kart.translateX(-moveDistance);
+
+        if (keyboard.pressed("space")) kart.position.set(0.0, 0.0, 2.0);
     }
 }
